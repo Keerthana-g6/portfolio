5ab84a2db68fda9dcb8b417c395636be67b52b59
Keerthana-g6 <keerthana.162g@gmail.com>
2026-03-26 22:37:07 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    # The main homepage
     path('', TemplateView.as_view(template_name="index.html"), name='index'),
-    
-    # Your project pages
     path('ecg/', TemplateView.as_view(template_name="ecg.html"), name='ecg'),
     path('rf/', TemplateView.as_view(template_name="rf.html"), name='rf'),
     path('portfolio/', TemplateView.as_view(template_name="portfolio.html"), name='portfolio'),
-    
-    # Don't forget to map the rest of your project pages too!
     path('circuit-solver/', TemplateView.as_view(template_name="circuit-solver.html"), name='circuit-solver'),
     path('tracker/', TemplateView.as_view(template_name="tracker.html"), name='tracker'),
     path('planner/', TemplateView.as_view(template_name="planner.html"), name='planner'),
     path('debug/', TemplateView.as_view(template_name="debug.html"), name='debug'),
 ]
+
+# Add these lines at the very bottom
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
