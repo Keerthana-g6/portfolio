bda05802a5954b160fc1972c8ea6f0218fa7059b
Keerthana-g6 <keerthana.162g@gmail.com>
2026-03-26 22:53:26 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,83 +1,18 @@
-from pathlib import Path
-import os
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = 'django-insecure-portfolio-key'
-
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
-
-# Application definition
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'core.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'core.wsgi.application'
-
-
-# Database (required but unused)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# Static files
-# Static files
-    
-
-# Where your raw images/css live right now
-# Tell Django where your original folders are
-# settings.py
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+
+# This tells Django where to look for your original files
 STATICFILES_DIRS = [
     BASE_DIR / 'images',
     BASE_DIR / 'assets',
     BASE_DIR / 'documents',
 ]
+
+# This is where Render will "collect" everything for the web
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Use the simpler WhiteNoise storage to avoid "Manifest" 404 errors
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-# WHERE KUBERNS WILL COLLECT THEM (Mandatory for deployment)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
